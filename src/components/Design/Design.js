a3ea2aa0e32f85ab3a24e6bb2c01de9e97e9fb99
MohammadAli MohammadKhani <mohammadali1379@gmail.com>
2024-03-04 11:52:13 +0330

    refactor: simplify code

--- src/components/Design/Design.js
+++ src/components/Design/Design.js
@@ -2,16 +2,10 @@ import React, { useState } from "react";
 import Logo from "../../images/Logo.svg";
 import "../index.css";
 // import { useLocation } from "react-router-dom";
-import { Button, Image } from "antd";
+import { Image } from "antd";
 import DesignCloudIcon from "../../images/DesignCloudIcon.svg";
 import GeneralList from "../GeneralList";
-import { EditOutlined } from "@ant-design/icons";
-import DesignInfoComponent from "./DesignInfoComponent";
-import DifficultyIcon from "../../images/HardIconForDesignPage.svg";
-import InnovationIcon from "../../images/InnovationIcon.svg";
-import DesignUploadComponent from "./DesignUploadComponent";
-import EnhanceIcon from "../../images/EnhanceIcon.svg";
-import EditIcon from "../../images/EditIcon.svg";
+import DesignRenderItem from "./DesignRenderItem";
 
 const Design = () => {
   // const { state } = useLocation();
@@ -118,201 +112,13 @@ const RenderItem = (item, idx) => {
     height: 40,
   };
   return (
-    <div
-      key={idx}
-      style={{
-        backgroundColor: "#F5F5F5",
-        width: "90%",
-        height: "90%",
-        padding: "1%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-      }}
-      className="rounded-l"
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          marginLeft: "2%",
-          marginRight: "2%",
-          marginBottom: "5%",
-          width: "90%",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <span style={{ fontWeight: "bolder", fontSize: 40 }}>Idea 1</span>
-
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <DesignInfoComponent
-              color="#00e15A"
-              title="Very High"
-              imgSrc={InnovationIcon}
-              infoStyle={infoStyle}
-            />
-
-            <DesignInfoComponent
-              color="#EA0054"
-              title="Hard"
-              imgSrc={DifficultyIcon}
-              infoStyle={infoStyle}
-            />
-          </div>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "95%",
-            }}
-          >
-            <span>
-              English idea comes from one of Seneca’s Epistles (58), written
-              about a.d. 64 during his retirement from Emperor Nero’s court, in
-              which the Roman philosopher uses idea in the sense of “Platonic
-              idea, eternal archetype.” Seneca wrote idea in Latin letters;
-              English idea comes from one of Seneca’s Epistles (58), written
-              about a.d. 64 during his retirement from Emperor Nero’s court, in
-              which the Roman philosopher uses idea in the sense of “Platonic
-              idea, eternal archetype.” Seneca wrote idea in Latin letters;
-              English idea comes from one of Seneca’s Epistles (58), written
-              about a.d. 64 during his retirement from Emperor Nero’s court, in
-              which the Roman philosopher uses idea in the sense of “Platonic
-              idea, eternal archetype.” Seneca wrote idea in Latin letters;
-              English idea comes from one of Seneca’s Epistles (58), written
-              about a.d. 64 during his retirement from Emperor Nero’s court, in
-              which the Roman philosopher uses idea in the sense of “Platonic
-              idea, eternal archetype.” Seneca wrote idea in Latin
-            </span>
-          </div>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingBottom: 10,
-          }}
-        ></div>
-      </div>
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "20%",
-          justifyContent: "space-around",
-          columnGap: 2,
-          rowGap: 2,
-        }}
-      >
-        <DesignUploadComponent />
-
-        {!isEditMode ? (
-          <Button
-            style={{
-              // backgroundColor: "#F4C6FF",
-              color: "#DE54FF",
-              height: "15%",
-              borderColor: "#F4C6FF",
-              borderWidth: 1,
-            }}
-            onClick={() => {
-              setIsEditMode(true);
-            }}
-            type="default"
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: 'center',
-                alignItems: "center",
-              }}
-            >
-              <Image
-                src={EditIcon}
-                height={25}
-                width={25}
-                style={{ marginRight: 12 }}
-                preview={false}
-              />
-              <span
-                style={{ fontWeight: "bolder", color: "#D32EFF", fontSize: 24 }}
-              >
-                Edit
-              </span>
-            </div>
-          </Button>
-        ) : (
-          <Button
-            style={{
-              backgroundColor: "#F4C6FF",
-              color: "#DE54FF",
-              height: "15%",
-            }}
-            icon={<EditOutlined width={120} height={120} />}
-            onClick={() => {
-              setIsEditMode(false);
-            }}
-          >
-            <span
-              style={{ fontWeight: "bolder", color: "#D32EFF", fontSize: 24 }}
-            >
-              Done
-            </span>
-          </Button>
-        )}
-
-        <Button
-          style={{
-            backgroundColor: "#DE54FF",
-            color: "white",
-            height: "15%",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-evenly",
-              alignItems: "center",
-            }}
-          >
-            <Image
-              src={EnhanceIcon}
-              height={25}
-              width={25}
-              style={{ marginRight: 12 }}
-              preview={false}
-            />
-            <span style={{ fontWeight: "bolder", fontSize: 24 }}>Enhance</span>
-          </div>
-        </Button>
-      </div>
-    </div>
+    <DesignRenderItem
+      infoStyle={infoStyle}
+      isEditMode={isEditMode}
+      setIsEditMode={setIsEditMode}
+      item={item}
+      idx={idx}
+    />
   );
 };
 

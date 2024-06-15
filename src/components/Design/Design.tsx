803bba0ee915d9ed99d7bef46d8b8b5cd1d112a6
MohammadAli MohammadKhani <mohammadali1379@gmail.com>
2024-06-15 11:26:40 +0330

    fix: api call in design

--- src/components/Design/Design.tsx
+++ src/components/Design/Design.tsx
@@ -20,13 +20,15 @@ const Design = () => {
 
   const navigate = useNavigate();
 
-  const assignmentID = localStorage.getItem('assignment_id')
+  const assignmentID = localStorage.getItem("assignment_id")!;
 
   const fetchDesignData = async () => {
     setSpinning(true);
     try {
       if (localStorage.getItem("token")) {
-        const response = await api.get(`api/questions/${assignmentID}`);
+        const response = await api.get("api/questions/", {
+          assignment: assignmentID,
+        });
         localStorage.setItem("questions", JSON.stringify(response.data));
         response.data.sort(function (x, y) {
           return x.is_selected_for_assignment === y.is_selected_for_assignment

4614f65612b24c2863ac3c49df194b79791d6f17
Sina Elahimanesh <sinaaelahimanesh@gmail.com>
2025-10-16 23:36:38 +0200

    fix issue of width

--- src/components/GeneralList.tsx
+++ src/components/GeneralList.tsx
@@ -1,15 +1,16 @@
 import { List } from "antd";
 
 const GeneralList = ({ data, RenderItem, numOfColumn }) => {
+  const listData = Array.isArray(data) ? data : [];
   return (
-      <List
-        grid={{
-          column: numOfColumn,
-          gutter: numOfColumn === 2 ? 16 : 0,
-        }}
-        dataSource={data}
-        renderItem={RenderItem}
-      />
+    <List
+      grid={{
+        column: numOfColumn,
+        gutter: numOfColumn === 2 ? 16 : 0,
+      }}
+      dataSource={listData}
+      renderItem={(item, idx) => <List.Item style={{ width: "100%" }}>{RenderItem(item, idx)}</List.Item>}
+    />
   );
 };
 

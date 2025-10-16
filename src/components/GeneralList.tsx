0789c3f394460b00c8770bd5aff1a74af9a9a011
Sina Elahimanesh <sinaaelahimanesh@gmail.com>
2025-10-17 00:00:50 +0200

    width issue

--- src/components/GeneralList.tsx
+++ src/components/GeneralList.tsx
@@ -1,15 +1,18 @@
 import { List } from "antd";
 
 const GeneralList = ({ data, RenderItem, numOfColumn }) => {
+  const listData = Array.isArray(data) ? data : [];
   return (
-      <List
-        grid={{
-          column: numOfColumn,
-          gutter: numOfColumn === 2 ? 16 : 0,
-        }}
-        dataSource={Array.isArray(data) ? data : []}
-        renderItem={RenderItem}
-      />
+    <List
+      grid={{
+        column: numOfColumn,
+        gutter: numOfColumn === 2 ? 16 : 0,
+      }}
+      dataSource={listData}
+      renderItem={(item, idx) => (
+        <List.Item style={{ width: "100%" }}>{RenderItem(item, idx)}</List.Item>
+      )}
+    />
   );
 };
 
